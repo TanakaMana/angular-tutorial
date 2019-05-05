816f94d563eae450a3b4071a0951be2043ce7239
TanakaMana <makkuromyaon@gmail.com>
2019-05-05 16:03:05 +0900

    [create] 役員一覧を作成

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Member } from './member';
+import { MemberService} from './member.service';
 
 @Component({
   selector: 'my-dashboard',
-  template: `
-    <h3>ダッシュボード</h3>
-  `
+  templateUrl: './dashboard.component.html'
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  // 以下service利用に必要
+  members: Member[];
+  constructor(private memberService: MemberService) {}
+  ngOnInit() {
+    this.memberService.getMember()
+      .then(members => this.members = members.slice(1, 5));
+  }
+}
